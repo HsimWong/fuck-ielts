295b2255722820a7bb731dea77b90783a499ef1c
Xin Wang <hsimwong1202@gmail.com>
2021-04-09 01:58:20 +0800

    First commit: revising ielts

---
+++ recite.py
@@ -0,0 +1,172 @@
+'''
+Eng->Chn: 1
+Chn->Eng: 1
+Check Eng->Chn: pass 1
+Check Chn->Eng: pass 1
+'''
+daily_items = 3
+scanned_words = {}
+new_word_list = []   # [split:[words]]
+callback_list = {}
+today_recited = {}
+today_not_remembered = {}
+
+
+import json 
+import random
+
+def load_history():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    with open("history.json", 'r') as f:
+        raw_content = str(f.read())
+        # print(raw_content)
+        if len(raw_content) == 0:
+            return 
+        scanned_words = json.loads(raw_content)
+
+def generate_new_list():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    with open('wordlist.json', 'r') as f:
+        new_word_list = json.loads(f.read())[len(scanned_words):len(scanned_words) + daily_items]
+
+
+def memorize_new_list():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    for word in new_word_list:
+        print()
+        print("含义：",word['meaning'], '\t', "词汇：",word['key'], "\t")
+        print("发音：",word['pronounciation'])
+        refined_word = word['key'][:-1] if word['key'][-1] == '*' else word['key']
+        while True:
+
+            word['recited_times'] += 1
+            entry = input("请拼写出整个单词：")
+            if entry == 'exit':
+                return
+            elif entry == refined_word:
+                callback_list.update({int(word['index']):word})
+                break
+            else:
+                word['mistakes'] += 1
+                today_not_remembered.update({int(word['index']): word})
+                callback_list.update({int(word['index']):word})
+                print("输入错误，请重新输入")
+                continue
+        print("你已经背完今天的生词了，我们回顾以下他们")
+
+        
+
+def generate_callbacks():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    for word_index in scanned_words.keys():
+        word = scanned_words[word_index]
+        if (word['mistakes'] != 0) and word['corrects'] / word['mistakes'] <= 2:
+            callback_list.update({int(word['index']):word})
+
+    
+
+def check_callbacks():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    # englist_list = {}
+    while True:
+        if len(callback_list) == 0:
+
+            break 
+        else:
+            keys = list(callback_list.keys())
+            random.shuffle(keys)
+            for word_index in keys:
+                word = callback_list[word_index]
+                refined_word = word['key'][:-1] if word['key'][-1] == '*' else word['key']
+                while True:
+                    word['recited_times'] += 1
+                    word['today_recited'] += 1
+                    print("含义：",word['meaning'])
+                    entry = input("请写出其英文：")
+                    if entry == 'exit':
+                        return
+                    elif entry == refined_word:
+                        word['corrects'] += 1
+                        scanned_words.update({int(word['index']): word})
+                        if word['today_recited'] >= 2 and (word['mistakes'] == 0 or \
+                            (word['mistakes'] != 0 and (word['corrects'] >= 2))):
+                            word['today_recited'] = 0
+
+                            scanned_words.update({int(word['index']): word})
+                            callback_list.pop(word_index)
+                        break 
+                    else:
+                        word['mistakes'] += 1
+                        scanned_words.update({int(word['index']):word})
+                        print("输入错误，请重新输入")
+                        continue
+                continue
+
+
+
+def save():
+    global daily_items
+    global scanned_words
+    global new_word_list
+    global callback_list
+    global today_not_remembered
+    global today_recited
+    print("祝贺！你已经完成了今天的任务")
+    history_file = str(json.dumps(scanned_words, ensure_ascii=False).encode('utf-8').decode('utf-8'))
+    with open('history.json', 'w') as f:
+        f.write(history_file)
+
+    with open('log.txt','w+') as f:
+        import datetime
+        f.write('\n')
+        f.write(str(datetime.datetime.now()))
+        f.write('\t')
+        f.write(str(daily_items - len(callback_list)))
+
+
+    
+
+def main():
+    load_history()
+    generate_callbacks()
+    generate_new_list()
+    memorize_new_list()
+    check_callbacks()
+    save()
+
+
+main()
+
+    
+
+    # wordlist = load_wordlist()
+    # recite_history()
+    # recite_new_list()
+    # update_history()
+
+    
